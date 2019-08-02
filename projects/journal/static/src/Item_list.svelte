01ff9d4ae37285f7e68f38534638e375adde9ec2
John Syrinek <john.syrinek@gmail.com>
2019-08-02 11:46:53 -0500

    Basic editing

--- projects/journal/static/src/Item_list.svelte
+++ projects/journal/static/src/Item_list.svelte
@@ -5,10 +5,32 @@
   if (!items.length) {
     items[items.length] = item("Do something");
   }
+
+  const remove_contents = parent =>
+    [...parent.childNodes].forEach(el => el.remove());
+  const edit = (item, element) => {
+    const { parentNode } = element;
+    remove_contents(parentNode);
+    const input = document.createElement("input");
+    parentNode.appendChild(input);
+    input.value = item.description;
+    input.focus();
+    const listener = input.addEventListener("blur", () => {
+      console.log("blur");
+      input.removeEventListener("onblur", listener);
+      save(item, input);
+    });
+  };
+  const save = (item, element) => {
+    item.description = element.value;
+    const { parentNode } = element;
+    remove_contents(parentNode);
+    parentNode.textContent = item.description;
+  };
 </script>
 
 <ul>
   {#each items as item}
-    <li>{item.description}</li>
+    <li on:click={e => edit(item, e.target)}>{item.description}</li>
   {/each}
 </ul>
--- projects/journal/static/src/main.js
+++ projects/journal/static/src/main.js
@@ -2,10 +2,10 @@ import Index from './Index.svelte'
 import { journal } from './data/journal.js'
 
 const index = new Index({
-	target: document.body,
-	props: {
+  target: document.body,
+  props: {
     journal: journal()
   }
-});
+})
 
-export default index;
+export default index
