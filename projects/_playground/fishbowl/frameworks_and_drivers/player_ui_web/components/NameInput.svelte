eb984ead6db10a92e35353f3bad041476e577ecd
John Syrinek <john.syrinek@gmail.com>
2021-01-15 07:45:06 -0600

    Fishbowl checkpoint (stale)

---
+++ projects/_playground/fishbowl/frameworks_and_drivers/player_ui_web/components/NameInput.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+    export let name = '';
+</script>
+<script>
+    import { onMount } from 'svelte'
+    const ls = window.localStorage
+    onMount(() => {
+        name = ls.getItem('name')
+    })
+
+    const changeName = () => {
+        ls.setItem('name', name)
+    }
+</script>
+<p><strong>Enter your name</strong></p>
+<input type="text" placeholder="John Doe" bind:value={name} on:change={changeName} /><br/>
